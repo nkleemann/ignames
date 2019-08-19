e271ed842629f1344f8abd616a03baf5feafa98f
nkleemann <n.kleemann@posteo.de>
2019-08-19 22:05:37 +0200

    waitgroup version

---
+++ ignames.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"sync"
+	"time"
+)
+
+//
+//	TYPES & CONSTANTS
+//
+
+// A generationMethod is a function that generates a string of a given length
+type generationMethod func(int) string
+
+// Using a third party service,
+const baseURL = "https://instagram.com/"
+
+// const baseURL = "https://insta-node.herokuapp.com/_validate_username?username="
+
+//
+//	USERNAME GENERATION
+//
+
+// Generate a instagram username of a given length based on a generation method
+func newUserName(genMethod generationMethod, length int) string {
+	return genMethod(length)
+}
+
+// Generate a random string
+func randomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+//
+//	NETWORKING
+//
+
+// Check if the username is valid
+func valid(status int) bool {
+	return status == 200
+}
+
+func checkName(name string, waitgroup *sync.WaitGroup) {
+	defer waitgroup.Done()
+	fmt.Printf("[*] Trying: %s\n", name)
+
+	resp, err := http.Get(baseURL + name)
+
+	if err != nil {
+		fmt.Println("Response error")
+		return false
+	}
+
+	if !valid(resp.StatusCode) {
+		return true
+	}
+
+	resp.Body.Close()
+	return false
+}
+
+//
+//	UTILITY
+//
+
+// SearchRandomNames searches for random usernames and returns valid ones in a list
+func SearchRandomNames(nameLength int, nameCount int) []string {
+
+	var waitgroup sync.WaitGroup
+	var validNames []string
+
+	// 3- and 4-letter names are all taken
+	if nameLength <= 4 {
+		return validNames
+	}
+
+	for i := 0; i < nameCount; i++ {
+		username := newUserName(randomString, nameLength)
+		waitgroup.Add(1)
+		go checkName(username, &waitgroup)
+	}
+
+	waitgroup.Wait()
+	return validNames
+}
+
+func main() {
+
+	fmt.Println("ignames v 0.0.1")
+	rand.Seed(time.Now().UnixNano())
+
+	validNames := SearchRandomNames(5, 10)
+	fmt.Println(validNames)
+}
